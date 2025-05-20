0bb2f62ed3a422490fffdd1517f29598be9be51d
Ardi Kharisma <ardikharisma836836@gmail.com>
2025-05-20 22:06:51 +0700

    main

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -4,9 +4,10 @@ import UserCard from "@/components/UserCard"
 const AdminPage = () => {
   return (
     <div className="flex p-4 flex-col gap-4 md:flex-row">
+
       {/* LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
-      {/* USERCHART */}
+        {/* USERCHART */}
         <div className="flex justify-between gap-4 flex-wrap">
           <UserCard type="student"/>
           <UserCard type="parent"/>
@@ -25,8 +26,10 @@ const AdminPage = () => {
           </div>
         </div>
       </div>
+      
       {/* RIGHT */}
       <div className="">r</div>
+      
     </div>
   )
 }
--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -1,19 +1,25 @@
-"use client"
+'use client'
 import Image from "next/image"
 import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
   {
-    name: '18-24',
-    uv: 31.47,
+    name: 'Total',
+    uv: 106,
     pv: 2400,
-    fill: '#8884d8',
+    fill: '#ffffff',
   },
   {
-    name: '25-29',
-    uv: 26.69,
+    name: 'Girls',
+    uv: 65,
     pv: 4567,
-    fill: '#83a6ed',
+    fill: '#FAE27C',
+  },
+  {
+    name: 'Boy',
+    uv: 79,
+    pv: 4567,
+    fill: '#c3ebfa',
   },
 ];
 
@@ -27,21 +33,20 @@ const style = {
 const CountChart = () => {
   return (
     <div className='bg-white flex p-5 rounded-2xl flex-col'>
-        <div className="flex justify-between">
-            <span className="text-xs font-medium">Student</span>
+        <div className="flex justify-between items-center">
+            <h1 className="text-lg font-medium">Student</h1>
             <Image src="/moreDark.png" alt="" width={20} height={20}/>
         </div>
-        <div className="w-full h-[75%]">
-            <ResponsiveContainer>
-                <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={data}>
+        <div className='relative w-full h-[75%]'>
+            <ResponsiveContainer width="100%" height={250}>
+                <RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={32} data={data}>
                 <RadialBar
-                    label={{ position: 'insideStart', fill: '#fff' }}
                     background
                     dataKey="uv"
                 />
-                <Legend iconSize={10} layout="vertical" verticalAlign="middle" />
                 </RadialBarChart>
             </ResponsiveContainer>
+            <Image src="/maleFemale.png" alt="" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
         </div>
         <div className="flex justify-center gap-5">
             <div className="flex flex-col">
