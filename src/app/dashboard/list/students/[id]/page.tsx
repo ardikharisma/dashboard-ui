c7ae8e6d7b5341efc58ecb0ff4f9693dd1b04a23
Ardi Kharisma <ardikharisma836836@gmail.com>
2025-05-27 22:39:58 +0700

    new commit

--- src/app/dashboard/list/students/[id]/page.tsx
+++ src/app/dashboard/list/students/[id]/page.tsx
@@ -4,7 +4,7 @@ import Perfomence from "@/components/Perfomence"
 import Image from "next/image"
 import Link from "next/link"
 
-const SingleTeacherPage = () => {
+const SingleStudentPage = () => {
   return (
      <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
       {/* LEFT */}
@@ -15,7 +15,7 @@ const SingleTeacherPage = () => {
           <div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
             <div className="w-1/3">
               <Image
-                src="https://images.pexels.com/photos/428328/pexels-photo-428328.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                src="https://images.pexels.com/photos/1187765/pexels-photo-1187765.jpeg?auto=compress&cs=tinysrgb&w=1200"
                 alt=""
                 width={144}
                 height={144}
@@ -25,7 +25,7 @@ const SingleTeacherPage = () => {
             <div className="w-2/3 flex flex-col justify-between gap-4">
               <div className="flex items-center gap-4">
                 <h1 className="text-xl font-semibold">
-                  Ujang Sunarto
+                  Ucup Gunawan
                 </h1>
               </div>
               <p className="text-sm text-gray-500">
@@ -118,7 +118,7 @@ const SingleTeacherPage = () => {
         </div>
         {/* BOTTOM */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-          <h1>Teacher&apos;s Schedule</h1>
+          <h1>Student&apos;s Schedule</h1>
             <BigCalendar/>
         </div>
       </div>
@@ -127,11 +127,11 @@ const SingleTeacherPage = () => {
         <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-              <Link className="p-3 rounded-md bg-lamaSkyLight" href="">Teacher&apos;s Classes</Link>
-              <Link className="p-3 rounded-md bg-lamaPurpleLight" href="">Teacher&apos;s Classes</Link>
-              <Link className="p-3 rounded-md bg-lamaSkyLight" href="">Teacher&apos;s Classes</Link>
-              <Link className="p-3 rounded-md bg-lamaYellowLight" href="">Teacher&apos;s Classes</Link>
-              <Link className="p-3 rounded-md bg-lamaSkyLight" href="">Teacher&apos;s Classes</Link>
+              <Link className="p-3 rounded-md bg-lamaSkyLight" href="">Student&apos;s Classes</Link>
+              <Link className="p-3 rounded-md bg-lamaPurpleLight" href="">Student&apos;s Classes</Link>
+              <Link className="p-3 rounded-md bg-lamaSkyLight" href="">Student&apos;s Classes</Link>
+              <Link className="p-3 rounded-md bg-lamaYellowLight" href="">Student&apos;s Classes</Link>
+              <Link className="p-3 rounded-md bg-lamaSkyLight" href="">Student&apos;s Classes</Link>
           </div>
         </div>
         <Perfomence/> 
@@ -141,4 +141,4 @@ const SingleTeacherPage = () => {
   )
 }
 
-export default SingleTeacherPage
+export default SingleStudentPage
